022b128af1a16b66b081ddcd9b38551040a7527b
torkantos <torkantos@yahoo.com>
2025-10-26 22:50:05 -0500

    Nearly there: fucking with sorting dictionaries and lists of tuples

--- stats.py
+++ stats.py
@@ -20,3 +20,16 @@ def character_count(book_text):
             letter_counts[lowercase_letter] = 1
     return letter_counts
 
+def sort_on(items):
+    return items["num"]
+
+def report(dict):
+    book = get_book_text("books/frankenstein.txt")
+    letter_dict = character_count(book)
+    dict_list = []]
+    for item in letter_dict.items():
+        if item.isalpha():
+            dict_list.append({"char":item, "num": count})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+    
